619857f563245670a6d4b1fcbefc38c683b3de48
Andreas Lust <andreaslust@Admins-iMac-2.local>
2019-02-19 13:59:13 +0100

    added detailed card view

--- app/src/main/java/com/example/speisekarte/MainActivity.java
+++ app/src/main/java/com/example/speisekarte/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.speisekarte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.Window;
 import android.view.WindowManager;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.Spagetthi)
+    protected CardView spagetthi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +21,7 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/speisekarte/RecyclerLayoutManager.java
@@ -0,0 +1,14 @@
+package com.example.speisekarte;
+
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+
+public class RecyclerLayoutManager {
+
+    @BindView(R.id.rv_cardviews)
+    protected RecyclerView rv;
+
+
+
+}
